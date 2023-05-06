13b5dcd03135e45bb20bb9c5ceae1a5000fc3ae3
Channelal2001 <125389432+Channelal2001@users.noreply.github.com>
2023-05-06 13:43:12 +0200

    HomeFragment changes

--- app/src/main/java/com/example/newsocialgift/fragments/HomeFragment.java
+++ app/src/main/java/com/example/newsocialgift/fragments/HomeFragment.java
@@ -6,7 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
 
 import androidx.annotation.DrawableRes;
 import androidx.fragment.app.Fragment;
