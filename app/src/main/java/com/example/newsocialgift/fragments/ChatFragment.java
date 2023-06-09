06d5e607659d0e00a403754095147d74859fb9b7
Channelal2001 <125389432+Channelal2001@users.noreply.github.com>
2023-06-09 11:55:17 +0200

    Updates

--- app/src/main/java/com/example/newsocialgift/fragments/ChatFragment.java
+++ app/src/main/java/com/example/newsocialgift/fragments/ChatFragment.java
@@ -4,9 +4,12 @@ import static android.content.Context.MODE_PRIVATE;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,7 +17,6 @@ import androidx.annotation.DrawableRes;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.volley.AuthFailureError;
@@ -25,18 +27,21 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
 import com.example.newsocialgift.ChatModel;
-import com.example.newsocialgift.Gift;
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+
 import com.example.newsocialgift.Message;
 import com.example.newsocialgift.MessageLayoutManager;
 import com.example.newsocialgift.R;
 import com.example.newsocialgift.User;
-import com.example.newsocialgift.Wishlist;
 import com.example.newsocialgift.adapters.ChatAdapter;
 import com.google.gson.Gson;
 
-import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -44,11 +49,15 @@ import java.util.Map;
 
 public class ChatFragment  extends Fragment {
     private static final String ARG_ICON = "ARG_ICON";
+    private Button backButton;
     private ImageView profileImage;
     private TextView profileName;
+    private EditText messageEditText;
+    private Button sendButton;
     private RecyclerView mRecyclerView;
     private ChatAdapter mAdapter;
     private List<ChatModel> mData = new ArrayList<>();
+    private Socket socket;
 
     private interface MessagesCallback {
         void onSuccess(Message[] messages);
@@ -84,9 +93,122 @@ public class ChatFragment  extends Fragment {
         Bundle arguments = getArguments();
         String otherUserID = arguments.getString("userID");
 
+        backButton = view.findViewById(R.id.backButton);
         profileImage = view.findViewById(R.id.profileImage);
         profileName = view.findViewById(R.id.profileName);
+        messageEditText = view.findViewById(R.id.message);
+        sendButton = view.findViewById(R.id.sendButton);
         mRecyclerView = view.findViewById(R.id.recyclerView);
+
+        String token = preferences.getString("token", "");
+        try {
+            IO.Options options = new IO.Options();
+            options.path = "/i3/socialgift/socket.io";
+            socket = IO.socket("https://balandrau.salle.url.edu", options);
+            socket.emit("login", token);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return view;
+        }
+
+        if (socket != null) {
+            Log.e("mySocket", "is not null and not undefined");
+
+            socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    Log.i("Socket", "Connected to server");
+                    Log.i("Socket", socket.id());
+                    socket.emit("login", token);
+                }
+            });
+
+            socket.on("save_msg", new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    // TODO: Obtenir les dades del missatge enviat i mostrar-lo per pantalla
+                    JSONObject saveMsg = (JSONObject) args[0];
+                    // Update UI or perform any necessary actions with the message
+                    Log.i("Socket", "Mensaje recibido: " + saveMsg);
+                }
+            });
+
+            socket.on("send_msg", new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    String sendMsg = (String) args[0];
+                    Log.i("Socket", "sendMsg => " + sendMsg);
+                }
+            });
+
+            socket.on("query_user", new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    String queryUser = (String) args[0];
+                    Log.i("Socket", "queryUser => " + queryUser);
+                }
+            });
+
+            socket.on("historic_msg", new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    String historicMsg = (String) args[0];
+                    Log.i("Socket", "historicMsg => " + historicMsg);
+                }
+            });
+
+            socket.on("new_msg", new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    try {
+                        // TODO: Obtenir les dades del missatge del paràmetre args i mostrar-ho per pantalla
+                        String newMsg = (String) args[0];
+                        // Update UI or perform any necessary actions with the message
+                        Log.i("Socket", "newMsg => " + newMsg);
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            socket.on("connect_error", new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    Exception error = (Exception) args[0];
+                    Log.e("Socket", "TransportError: " + error.getMessage());
+                }
+            });
+
+            socket.on("login", new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    String login = (String) args[0];
+                    Log.i("Socket", "login => " + login);
+                }
+            });
+
+            socket.on("disconnect", new Emitter.Listener() {
+                @Override
+                public void call(Object... args) {
+                    String reason = (String) args[0];
+                    Log.i("Socket", "disconnect:");
+                    // Reconnect logic
+                    new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                Thread.sleep(1000);
+                                socket.connect();
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }).start();
+                }
+            });
+        } else {
+            Log.e("mySocket", "is null or undefined");
+        }
 
         loadUser(otherUserID);
         loadMessages(otherUserID, new MessagesCallback() {
@@ -106,16 +228,35 @@ public class ChatFragment  extends Fragment {
             }
         });
 
-        /*mData.add(new ChatModel(otherUserID, "Hola"));
-        mData.add(new ChatModel(userID, "Como estas?"));
-        mData.add(new ChatModel(userID, "Bien y tu?"));
-        mData.add(new ChatModel(otherUserID, "Bien y tu?"));
-        mData.add(new ChatModel(userID, "Bien"));
-        mAdapter = new ChatAdapter(mData, userID);
-        mRecyclerView.setLayoutManager(new MessageLayoutManager(getContext(), userID, mRecyclerView));
-        mRecyclerView.setAdapter(mAdapter);*/
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = messageEditText.getText().toString();
+                if (!message.isEmpty()) {
+                    sendMessage(message, userID);
+                    messageEditText.setText("");
+                }
+            }
+        });
 
         return view;
+    }
+
+    private void sendMessage(String message, String userID) {
+        SharedPreferences preferences = getActivity().getSharedPreferences("SocialGift", MODE_PRIVATE);
+        String token = preferences.getString("token", "");
+
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put("token", token);
+            jsonObject.put("reciver", userID);
+            jsonObject.put("message", message);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        socket.emit("query_user", jsonObject);
+        socket.emit("save_msg", jsonObject);
     }
 
     private void loadUser(String otherUserID) {
